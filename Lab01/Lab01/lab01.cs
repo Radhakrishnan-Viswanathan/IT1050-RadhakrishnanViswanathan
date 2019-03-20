beced88fab16e8b3c6bf6a07d122480690ba18e0
Radhakrishnan Viswanathan <S01211388@acad.tri-c.edu>
2019-03-19 21:38:22 -0400

    Created Lab01 Project

---
+++ Lab01/Lab01/lab01.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class test
+    {
+        static void Main(string[] args)
+        {
+            string greeting = "Hello World";
+            string firstName = "Radhakrishnan";
+            string lastName = "Viswanathan";
+            Console.WriteLine(greeting + " from " + firstName + " " + lastName);
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
